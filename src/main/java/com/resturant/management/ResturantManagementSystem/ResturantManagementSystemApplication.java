c06344605e8803517d5e2cf0392d9971d17c8199
Orl-Tokata <orltokata@gmail.com>
2025-10-06 22:28:22 +0700

    first commit

---
+++ src/main/java/com/resturant/management/ResturantManagementSystem/ResturantManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.resturant.management.ResturantManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResturantManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResturantManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resturant/management/ResturantManagementSystem/ResturantManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resturant.management.ResturantManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResturantManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
